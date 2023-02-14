597370cc1361c4aca43b04c0ac8f4e6ce4bac193
sofiwolfes <sofiwolfes@yahoo.es>
2023-02-13 19:40:20 -0500

    Fixed bug with the Challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -4,7 +4,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -14,3 +14,120 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Build a menu
+
+var baseMaps = {
+    "Streets": streets,
+};
+
+var tectonicplates = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
+
+var overlays = {
+    "Tectonic Plates": tectonicplates,
+    Earthquakes: earthquakes
+};
+
+L.control.layers(baseMaps, overlays).addTo(map);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+
+    function getColor(depth) {
+        switch (true) {
+            case depth > 90:
+                return "#ea2c2c";
+            case depth > 70:
+                return "#ea822c";
+            case depth > 50:
+                return "#ee9c00";
+            case depth > 30:
+                return "#eecc00";
+            case depth > 10:
+                return "#d4ee00";
+            default:
+                return "#98ee00";
+        }
+    }
+
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+
+        return magnitude * 4;
+    }
+
+    L.geoJson(data, {
+        // We turn each feature into a circleMarker on the map.
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo,
+
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                "Magnitude: "
+                + feature.properties.mag
+                + "<br>Depth: "
+                + feature.geometry.coordinates[2]
+                + "<br>Location: "
+                + feature.properties.place
+            );
+        }
+    }).addTo(earthquakes);
+
+    earthquakes.addTo(map)
+
+    var legend = L.control({
+        position: "bottomright"
+    });
+
+    legend.onAdd = function () {
+        var div = L.DomUtil.create("div", "info legend");
+
+        var grades = [-10, 10, 30, 50, 70, 90];
+        var colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"];
+
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML += "<i style='background: "
+                + colors[i]
+                + "'></i> "
+                + grades[i]
+                + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+
+    legend.addTo(map);
+
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (platedata) {
+
+        L.geoJson(platedata, {
+            color: "orange",
+            weight: 2
+        }).addTo(tectonicplates);
+
+        tectonicplates.addTo(map);
+    });
+
+});
+
